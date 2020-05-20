4e512e775093de7cf406cefc5393769b43fa6560
Roman <tinkotsu@gmail.com>
2020-05-20 15:14:17 +0500

    added 6 tasks

--- task_5.py
+++ task_5.py
@@ -1,2 +1,15 @@
 from itertools import cycle, count
 
+for i in count(int(input('Введите начало отсчета: '))):  # решение первой части задания
+    if i > 100:  # останавливаю цикл, если досчитали до 100
+        break
+    print(i)
+
+
+my_list = input('Введите элементы списка через пробел: ').split()  # решение второй части задания
+for elem in cycle(my_list):
+    print(elem)
+    if my_list.index(elem) == len(my_list) - 1:
+        if input('Для продолжения введите любой символ. Для выхода введите \'q\': ') == 'q':
+            break
+
