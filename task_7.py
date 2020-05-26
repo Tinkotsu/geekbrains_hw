1a382c6a7500579fe0d9d9e14ee335c75a01f684
Roman <tinkotsu@gmail.com>
2020-05-26 10:16:00 +0500

    finished hw

---
+++ task_7.py
@@ -0,0 +1,17 @@
+import json
+
+firm_dict = dict()
+avg_dict = dict()
+with open('task_7.txt') as f:
+    i = 0
+    f_sum = 0
+    for line in f.readlines():
+        i += 1
+        name, firm_t, income, costs = line.split()
+        profit = int(income) - int(costs)
+        firm_dict.update({name: profit})
+        if profit > 0:
+            f_sum += profit
+    avg_dict.update({"average_profit": f_sum / i})
+with open('task_7_ans.json', 'w', encoding='utf8') as f:
+    json.dump([firm_dict, avg_dict], f, ensure_ascii=False)
