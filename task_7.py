464f79d1b2a8470fea85588d59c1f792d85a1419
Roman <tinkotsu@gmail.com>
2020-05-26 10:39:36 +0500

    some fixes

--- task_7.py
+++ task_7.py
@@ -6,11 +6,11 @@ with open('task_7.txt') as f:
     i = 0
     f_sum = 0
     for line in f.readlines():
-        i += 1
         name, firm_t, income, costs = line.split()
         profit = int(income) - int(costs)
         firm_dict.update({name: profit})
         if profit > 0:
+            i += 1
             f_sum += profit
     avg_dict.update({"average_profit": f_sum / i})
 with open('task_7_ans.json', 'w', encoding='utf8') as f:
